96d70c4dc2803a1b5741d5bfaef740bb8b44b000
cyrus2021 <cyrus@ajency.in>
2018-01-23 19:51:44 +0530

    build updated

--- client/dist/assets/js/globals.js
+++ client/dist/assets/js/globals.js
@@ -1,6 +1,7 @@
 (function(win){
     win.VL_SMS_GLOBALS = {
         dateFormat: 'YYYY-MM-DDTHH:mm:ss',
+        // apiUrl: 'http://127.0.0.1:8000'
         apiUrl: 'http://localhost:3000'
     };
 
