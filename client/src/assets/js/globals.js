2761d479fc3365f45d28aaa6fd31fbb01a07369c
Ajaj Rajguru <ajaj@ajency.in>
2018-01-23 13:32:51 +0530

    loader centre aligned

--- client/src/assets/js/globals.js
+++ client/src/assets/js/globals.js
@@ -1,7 +1,8 @@
 (function(win){
     win.VL_SMS_GLOBALS = {
         dateFormat: 'YYYY-MM-DDTHH:mm:ss',
-        apiUrl: 'http://127.0.0.1:8000'
+        // apiUrl: 'http://127.0.0.1:8000'
+        apiUrl: 'http://localhost:3000'
     };
 
 })(window);
