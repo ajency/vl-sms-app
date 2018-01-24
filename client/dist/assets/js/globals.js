d9b05b12cdeb1d3200291c3bb21356ab14cebc94
cyrus2021 <cyrus@ajency.in>
2018-01-24 19:07:50 +0530

    build with live url

--- client/dist/assets/js/globals.js
+++ client/dist/assets/js/globals.js
@@ -1,8 +1,8 @@
 (function(win){
     win.VL_SMS_GLOBALS = {
         dateFormat: 'YYYY-MM-DDTHH:mm:ss',
-        // apiUrl: 'http://sms.foliageoutdoors.com/rest/api',
-        apiUrl: 'http://localhost:3000/v1/api'
+        apiUrl: 'http://sms.foliageoutdoors.com/rest/api',
+        // apiUrl: 'http://localhost:3000/v1/api'
     };
 
 })(window);
