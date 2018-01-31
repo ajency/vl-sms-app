13cec03adb7ad17ea5caf57b37ec522a80bcc562
sairajgadekar <sairaj@ajency.in>
2018-01-31 16:12:12 +0530

    phone type changes in trip passenger api

--- server/app/Http/Controllers/ExternalApiController.php
+++ server/app/Http/Controllers/ExternalApiController.php
@@ -10,7 +10,7 @@ class ExternalApiController extends Controller
 {
     protected $request;
 
-    public function __construct(Client $client,Request $request)
+    public function __construct(Client $client, Request $request)
     {
         // $this->request = $request;
 
@@ -22,13 +22,13 @@ class ExternalApiController extends Controller
 
     public function trips(Request $request)
     {
-        /*$globalhelper=new GlobalHelper;    
+        /*$globalhelper=new GlobalHelper;
         $user = $globalhelper->getUserApiSecret($this->request->user()->id);
-        
+
         if(empty($user)){
-          return [ "message" => "Unauthenticated."];
-        }   
-        */
+        return [ "message" => "Unauthenticated."];
+        }
+         */
 
         $final_data = array();
 
@@ -36,44 +36,37 @@ class ExternalApiController extends Controller
 
         $request_url = $this->api_url . '/admin/trips.json?ac_api_key=' . $this->api_key . '&user_secret=' . $this->user_secrete . '&count=true&totalCount=true&order_by[]=state.asc&order_by[]=code.asc&order_by[]=id.desc&search_fields[]=name&search_fields[]=code&search_fields[]=supplier_name';
 
-        
-        // set search query is passed 
-        if ($request->has('search')){
+        // set search query is passed
+        if ($request->has('search')) {
             $search_query = $request->input('search');
-            $request_url = $request_url .'&search=' . $search_query ;
-        }
-        else{
+            $request_url  = $request_url . '&search=' . $search_query;
+        } else {
             $search_query = '';
         }
 
-        
-        // set offset is passed 
-        if ($request->has('offset')){
+        // set offset is passed
+        if ($request->has('offset')) {
             $offset = $request->input('offset');
-        }
-        else{
+        } else {
             $offset = 0;
-        } 
-
-        if($offset){
-            $request_url = $request_url .'&offset=' . $offset ;
         }
-        
-        // set limit is passed 
-        if ($request->has('limit')){
+
+        if ($offset) {
+            $request_url = $request_url . '&offset=' . $offset;
+        }
+
+        // set limit is passed
+        if ($request->has('limit')) {
             $limit = $request->input('limit');
-        }
-        else{
+        } else {
             $limit = 100;
-        } 
-
-        if($limit){
-            $request_url = $request_url .'&limit=' . $limit ;
         }
-        
 
+        if ($limit) {
+            $request_url = $request_url . '&limit=' . $limit;
+        }
 
-        $res = $this->client->request('GET', $request_url );
+        $res = $this->client->request('GET', $request_url);
 
         $data = json_decode($res->getBody(), true);
 
@@ -88,21 +81,21 @@ class ExternalApiController extends Controller
         }
 
         return [
-            "status" => "success",
-            "msg"    => "ok",
-            "data"   => $final_data,
-            "count"  =>  $data['count'],
-            "totalCount" => $data['totalCount']  
+            "status"     => "success",
+            "msg"        => "ok",
+            "data"       => $final_data,
+            "count"      => $data['count'],
+            "totalCount" => $data['totalCount'],
         ];
     }
 
     public function departures(Request $request)
     {
-      /*  $globalhelper=new GlobalHelper;    
+        /*  $globalhelper=new GlobalHelper;
         $user = $globalhelper->getUserApiSecret($this->request->user()->id);
-        
+
         if(empty($user)){
-          return [ "message" => "Unauthenticated."];
+        return [ "message" => "Unauthenticated."];
         } */
 
         $filters = $request->input('filters');
@@ -114,7 +107,6 @@ class ExternalApiController extends Controller
         //        $res = $this->client->request('GET', $this->api_url . '/api/v1/admin/departures.json?ac_api_key=' . $user['api_key'] . '&user_secret=' . $user['user_secret'] . '&departure_from=' . $fromdate . '&limit=1000&trip_ids=' . $trip_id);
 
         $res = $this->client->request('GET', $this->api_url . '/api/v1/admin/departures.json?ac_api_key=' . $this->api_key . '&user_secret=' . $this->user_secrete . '&departure_from=' . $fromdate . '&limit=1000&trip_ids=' . $trip_id);
-
 
         $data = json_decode($res->getBody(), true);
 
@@ -140,13 +132,13 @@ class ExternalApiController extends Controller
     }
 
     public function participants(Request $request)
-    {   
-       /* $globalhelper=new GlobalHelper;    
+    {
+        /* $globalhelper=new GlobalHelper;
         $user = $globalhelper->getUserApiSecret($this->request->user()->id);
-        
+
         if(empty($user)){
-          return [ "message" => "Unauthenticated."];
-        }*/ 
+        return [ "message" => "Unauthenticated."];
+        }*/
 
         $departure_id = $request->input('departure_id');
 
@@ -167,9 +159,9 @@ class ExternalApiController extends Controller
 
         $data = json_decode($res->getBody(), true);
         //print_r($data);
-        $final_data = array();
+        $final_data             = array();
         $primary_contact_person = array();
-        
+
         $primary_alt_contact = array();
 
         foreach ($data['bookings'] as $dvalue) {
@@ -194,13 +186,12 @@ class ExternalApiController extends Controller
                     "passenger_name"    => $name_pri,
                     "primary"           => true,
                     "phone_no"          => $phone_pri,
-                    "phone_type"        => '',
+                    "phone_type"        => 'Phone number',
                     "booking_status"    => $booking_status,
                     "redundant_contact" => false,
                 ];
 
                 $final_data[] = $primary_contact_person;
-
 
                 //alt phoneno -data
                 if ($phone_alt_pri != '' && $phone_alt_pri != null) {
@@ -211,12 +202,59 @@ class ExternalApiController extends Controller
                         "passenger_name"    => $name_pri,
                         "primary"           => true,
                         "phone_no"          => $phone_alt_pri,
-                        "phone_type"        => '',
+                        "phone_type"        => 'Phone number alt',
                         "booking_status"    => $booking_status,
                         "redundant_contact" => true,
                     ];
 
                     $final_data[] = $primary_alt_contact;
+                }
+
+                /**
+                 * additional phone no for primary contact
+                 */
+                if (!empty($dvalue['custom_form_values'])) {
+                    $additional_primary_details = array();
+
+                    foreach ($dvalue['custom_form_values'] as $cus_key => $cus_value) {
+
+                        if (strpos($cus_key, 'vl_phone') !== false) {
+
+                            $phone_type = isset($data['booking_custom_forms']['']);
+
+                            if (isset($cus_value['country_code']) && isset($cus_value['number'])) {
+                                $additional_primary_details['vl_phone'] = $cus_value['country_code'] . $cus_value['number'];
+                                if (!isset($additional_primary_details['vl_first_name'])) {
+
+                                    $additional_primary_details['vl_first_name'] = $additional_primary_details['vl_phone'];
+                                    $additional_primary_details['vl_last_name']  = '';
+                                    $additional_phone_type                       = '';
+                                    foreach ($data['booking_custom_forms'] as $booking_custom_value) {
+                                        $additional_phone_type = isset($booking_custom_value['json_schema'][$cus_key]['title']) ? $booking_custom_value['json_schema'][$cus_key]['title'] : "Country code and phone number";
+                                    }
+
+                                    $additional_primary_details['vl_phone_type'] = $additional_phone_type;
+
+                                }
+                            }
+                        }
+                    }
+
+                    if (!empty($additional_primary_details)) {
+                        $final_additional_primary_data = [
+                            "booking_id"        => $dvalue['booking_ref'],
+                            "booking_ref_url"   => $booking_ref_url,
+                            "passenger_name"    => $additional_primary_details['vl_first_name'] . ' ' . $additional_primary_details['vl_last_name'],
+                            "primary"           => false,
+                            "phone_no"          => isset($additional_primary_details['vl_phone']) ? $additional_primary_details['vl_phone'] : "",
+                            "phone_type"        => '',
+                            "booking_status"    => $booking_status,
+                            "redundant_contact" => '',
+                        ];
+
+                        $final_data[] = $final_additional_primary_data;
+
+                    }
                 }
 
             }
@@ -226,6 +264,7 @@ class ExternalApiController extends Controller
 
                     if (!empty($passenger_val['custom_form_values'])) {
                         $other_passenger_details = array();
+
                         foreach ($passenger_val['custom_form_values'] as $cus_key => $cus_value) {
 
                             if (strpos($cus_key, 'vl_full_name_with_title') !== false) {
@@ -246,34 +285,40 @@ class ExternalApiController extends Controller
 
                             } else if (strpos($cus_key, 'vl_phone') !== false) {
 
-                                if(isset( $cus_value['country_code']) && isset($cus_value['number'])){
+                                if (isset($cus_value['country_code']) && isset($cus_value['number'])) {
                                     $other_passenger_details['vl_phone'] = $cus_value['country_code'] . $cus_value['number'];
                                     if (!isset($other_passenger_details['vl_first_name'])) {
                                         $other_passenger_details['vl_first_name'] = $other_passenger_details['vl_phone'];
                                         $other_passenger_details['vl_last_name']  = '';
                                     }
+
+                                    $other_phone_type      = '';
+                                    $additional_phone_type = isset($data['pax_custom_form']['json_schema'][$cus_key]['title']) ? $data['pax_custom_form']['json_schema'][$cus_key]['title'] : "Country code and phone number";
+
+                                    $other_passenger_details['vl_phone_type'] = $additional_phone_type;
+
                                 }
 
                             }
 
                         }
-                        if (!empty($other_passenger_details) ) {
+                        if (!empty($other_passenger_details)) {
                             $final_other_passenger_data = [
                                 "booking_id"        => $dvalue['booking_ref'],
                                 "booking_ref_url"   => $booking_ref_url,
                                 "passenger_name"    => $other_passenger_details['vl_first_name'] . ' ' . $other_passenger_details['vl_last_name'],
                                 "primary"           => false,
                                 "phone_no"          => isset($other_passenger_details['vl_phone']) ? $other_passenger_details['vl_phone'] : "",
-                                "phone_type"        => '',
+                                "phone_type"        => isset($other_passenger_details['vl_phone_type'])?$other_passenger_details['vl_phone_type']:"",
                                 "booking_status"    => $booking_status,
-                                "redundant_contact" => '',
+                                "redundant_contact" => "",
                             ];
 
-                            if ( ($final_other_passenger_data['phone_no'] != $primary_contact_person['phone_no']) || ( isset($primary_alt_contact['phone_no']) and $final_other_passenger_data['phone_no'] != $primary_alt_contact['phone_no']) ){
+                            // if ( ($final_other_passenger_data['phone_no'] != $primary_contact_person['phone_no']) || ( isset($primary_alt_contact['phone_no']) and $final_other_passenger_data['phone_no'] != $primary_alt_contact['phone_no']) ){
 
-                                $final_data[] = $final_other_passenger_data;
+                            $final_data[] = $final_other_passenger_data;
 
-                            }
+                            // }
                         }
                     }
                 }
