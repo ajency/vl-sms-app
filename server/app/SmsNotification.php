52fe7fe36634e7f62f69798bea3142d10908a79f
sairajgadekar <sairaj@ajency.in>
2018-02-07 19:48:32 +0530

    current between start end date check

--- server/app/SmsNotification.php
+++ server/app/SmsNotification.php
@@ -8,18 +8,22 @@ class SmsNotification extends Model
 {
     public function getPublishedNotification($limit, $offset, $search_query)
     {
-        $sms_notifications = SmsNotification::join('sms_notification_trips', 'sms_notification_trips.trip_id', '=', 'sms_notifications.trip_id');
+        $current_date=date('Y-m-d');
+
+        $sms_notifications = SmsNotification::join('sms_notification_trips', 'sms_notification_trips.trip_id', '=', 'sms_notifications.trip_id')
+        ->join('sms_notification_departures','sms_notification_departures.departure_id','=','sms_notifications.departure_id')
+        ->whereRaw("'".$current_date."' between date(starts_at) and date(ends_at)");
 
         if (trim($search_query) != '') {
             $sms_notifications->where('name','like',"%$search_query%")->orWhere('code','like',"%$search_query%");
         }
 
-       return $sms_notifications->select('sms_notification_trips.trip_id as id', 'code', 'name')
+        return $sms_notifications->select('sms_notification_trips.trip_id as id', 'code', 'name')
             ->distinct('sms_notifications.trip_id')
             ->limit($limit)
             ->offset($offset)
             ->get()
             ->toArray();
-
+       
     }
 }
